3549b5d32149834593afaa1eb941698e67ef3afe
FranCalda <90873698+FranCalda@users.noreply.github.com>
2022-05-20 16:30:34 -0300

    Proyecto JS

---
+++ JS/Clase 01/fundamentos/variables.js
@@ -0,0 +1,13 @@
+//Declarar una variable
+var total;
+
+//Asignar valor a una variable 
+
+total = 100;
+
+// Declarar y asignar valor a una variable
+
+var nombre = "Paco";
+
+var datos = {nombre:"juan", edad:25};
+var datos = null; //Limpio la variable
